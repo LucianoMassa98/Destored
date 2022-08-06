aba19caddd1da3c52df843220a3b66973ca171dc
Luciano Massa <lgmassa98@gmail.com>
2022-08-06 19:58:12 -0300

    prueba 3

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,4 +1,4 @@
-const { Sequelize } = require('sequelize');
+const  Sequelize  = require('sequelize');
 
 const { config } = require('./../config/config');
 const setupModels = require('./../db/models');
