ac4952b0705dd076756c0519560690e805e17433
lucianomassa98 <lgmassa98@gmail.com>
2023-10-03 20:58:49 -0300

    conexionBaseDatos

--- examples/pass-hash.js
+++
@@ -1,8 +0,0 @@
-const {bcrypt}=require('bcrypt');
-async function hashPassword(){
-  const myPassword='admin123';
-  const hash = await bcrypt.hash(myPassword);
-  console.log(hash)
-}
-
-hashPassword();
--- examples/pass-verify.js
+++
@@ -1,9 +0,0 @@
-const {bcrypt}=require('bcrypt');
-async function verifyPassword(){
-  const myPassword='admin123';
-  const hash='admin123';
-  const isMatch = await bcrypt.compare(myPassword,hash);
-  console.log(hash)
-}
-
-veryPassword();
